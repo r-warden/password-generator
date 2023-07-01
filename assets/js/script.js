0cf1d8155ad8172d7309fe3ef52f2ca8f29e1416
Richard W <richard.warden@protonmail.com>
2023-07-01 16:38:37 -0500

    added prompts for length and what types of characters to include

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@
 var generateBtn = document.querySelector('#generate');
 
 function generatePassword() {
-	
+	const length = Number(window.prompt("What length would you like your password (8 - 128)?"));
+	var lower = window.confirm("Include lower case letters?");
+	var upper = window.confirm("Include capital letters?");
+	var num = window.confirm("Include numerals?")
+	var special = window.confirm("Include special characters?");
+
 	return pw;
 }
 
