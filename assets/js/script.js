264ac764ab2d0a27b4bc49c0cc5a9b59dcb3a17e
Richard W <richard.warden@protonmail.com>
2023-07-01 16:13:49 -0500

    deleted a typo, switching to new branch for new feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
+function generatePassword() {
+	
+	return pw;
+}
+
 // Write password to the #password input
 function writePassword() {
 	var password = generatePassword();
